b344ad5b1ab73310a6d52fd6eb5efd7a6c3a2a08
mikek1337 <mikekegnlyew@gmail.com>
2022-06-30 06:24:30 -0700

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a mutilingual puzzle\n");
+
+	return (0);
+}
