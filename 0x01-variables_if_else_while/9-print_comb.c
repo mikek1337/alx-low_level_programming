fc1e4d918613d1e330532ead79fedc17cd613610
mikek1337 <mikekegnlyew@gmail.com>
2022-07-03 15:10:57 +0300

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	 int i = 0;
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		if (i == 9)
+		{
+			i++;
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
