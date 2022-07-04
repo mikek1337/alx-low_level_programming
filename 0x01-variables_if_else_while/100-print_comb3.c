7b9de7a8720ac596747023a699e981d11ac2f5a8
mikek1337 <mikekegnlyew@gmail.com>
2022-07-04 20:43:31 +0300

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	for (int i = 0; i < 9; i++)
+	 int i;
+	 int j;
+	for (i = 0; i < 9; i++)
 	{
-		for (int j = i + 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i % 10 + '0');
 			putchar(j % 10 + '0');
