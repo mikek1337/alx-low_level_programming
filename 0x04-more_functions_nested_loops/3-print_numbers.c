3d833da21b9353b18d9505acacf3ae4ca5460656
mikek1337 <mikekegnlyew@gmail.com>
2022-07-08 21:08:25 +0300

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
-	 while (i < 10)
-	 {
-		 _putchar(i++);
-	 }
-	 _putchar('\n');
-	 return (0);
+	 int i = 0;
+	while (i < 10)
+	{
+		_putchar(i++);
+	}
+	_putchar('\n');
+	return (0);
 }
