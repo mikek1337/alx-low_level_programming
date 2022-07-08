fb9d84034bb33fa88a6a8e72cb496ef6ab0e34e2
mikek1337 <mikekegnlyew@gmail.com>
2022-07-09 00:48:52 +0300

    why ?

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,8 +10,15 @@
 void print_diagonal(int n)
 {
 	 int i = n;
+	 int j;
 	while (i > 0)
 	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
 		_putchar('\\');
 		_putchar('\n');
 		i--;
