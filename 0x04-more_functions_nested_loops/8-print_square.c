a1cd4c94fc45ec817fe4b0312962534bbb832876
mikek1337 <mikekegnlyew@gmail.com>
2022-07-09 00:58:31 +0300

    why ?

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - prints # n number of times
+ *
+ * @size: number of times
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	 int i = 0;
+	while (i < n)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
