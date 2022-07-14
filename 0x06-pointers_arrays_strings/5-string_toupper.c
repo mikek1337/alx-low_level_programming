c780b7df0677c89c9ecd0f3989b6906afd2b7128
mikek1337 <mikekegnlyew@gmail.com>
2022-07-14 21:28:30 +0300

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase characters to uppercase
+ *
+ * @str: string to be converted
+ *
+ * Return: character pointer
+ */
+char *string_toupper(char *str)
+{
+	char c;
+	int i = 0;
+	c = *(str + i);
+	while (c != '\0')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			i++;
+			c = *(str + i);
+			continue;
+		}
+		else if (c >= 'a' && c <= 'z')
+		{
+			*(str + i) = c - 32;
+		}
+		i++;
+		c = *(str + i);
+	}
+	return (str);
+}
