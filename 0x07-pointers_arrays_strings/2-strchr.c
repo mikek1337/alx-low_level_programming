f56a0f2f7b367cc4c61ebd85aa1e7131ba20fd90
mikek1337 <mikekegnlyew@gmail.com>
2022-07-18 20:23:26 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,31 +12,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-	unsigned int found = 0;
-	char b;
-	char *temp;
-	b = *(s + i);
-	while (b != '\0')
+	while (*s != '\0')
 	{
-		printf("%c\n",b);
-		if(b == c)
+		if (*s == c)
 		{
-			temp = s;
-			c = *(temp + i);
-			
-			while (c != '\0')
-			{
-				printf("%c", c);
-				i++;
-				c = *(temp + i);
-				*(s + found) = c;
-				found++;
-			}
 			return (s);
 		}
-		i++;
-		b = *(s + i);
+		++s;
+	}
+
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (NULL);
 }
