9e3245dd58625af2ea82ee56e5c1702e77dae8d2
mikek1337 <mikekegnlyew@gmail.com>
2022-07-19 19:06:13 +0300

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void _putchar(char);
+void print_chessboard(char (*a)[8]);
