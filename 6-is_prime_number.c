ed41fbd7b1dc07f1535aeb5c964d287221978e63
mikek1337 <mikekegnlyew@gmail.com>
2022-07-20 19:55:31 +0300

    is prime

---
+++ 6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determines if a number is prime or not
+ * @n: the number to be evaluated
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	return (prime_number(n, 2));
+}
+
+/**
+ * prime_number - checks if the number is divisible
+ * @n: number
+ * @r: half the number
+ * Return: integer
+ */
+int prime_number(int n, int r)
+{
+	if (r >= n && n > 1)
+	{
+		return (1);
+	}
+	else if (n % r == 0 || n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime_number(n, r + 1));
+	}
+}
