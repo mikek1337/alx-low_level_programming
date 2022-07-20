0e6c3c015c819e3c40a4cafd2caa0a974481b6d1
mikek1337 <mikekegnlyew@gmail.com>
2022-07-19 23:37:06 -0700

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	 int i;
-	if (*s > '\0')
+	if (*s != '\0')
 	{
-		i +=  (_strlen_recursion(s)) + 1;
-		s++;
+		return (1 + (_strlen_recursion(s + 1)));
 	}
-	return (i);
+	else
+	{
+		return (0);
+	}
 }
