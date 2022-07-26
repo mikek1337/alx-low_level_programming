37528b793b4c3fb33838c42ae1e2d9b796bb22eb
mikek1337 <mikekegnlyew@gmail.com>
2022-07-26 19:00:11 +0300

    help

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,16 +13,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	 int s1_size = strlen(s1);
-	 int s2_size = strlen(s2);
-	 int total_size = s1_size + s2_size;
+	 int s1_size = 0;
+	 int s2_size = 0;
+	 int total_size = 0;
 	 int i;
 	 int j;
 	 char *concat;
+	 if (s1 != NULL)
+	 {
+		 s1_size = strlen(s1);
+	 }
+	 if (s2 != NULL)
+	 {
+		 s2_size = strlen(s2);
+	 }
+	total_size = s1_size + s2_size;
 	total_size = total_size - 2;
 	if (s1 == NULL && s2 == NULL)
 	{
-		return (NULL);
+		s1_size = 0;
+		s2_size = 0;
 	}
 	concat = malloc(sizeof(char) * total_size);
 	if (concat == NULL)
