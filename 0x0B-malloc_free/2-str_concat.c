1103cbd9e49407047dfbe361fd1f015816037a84
mikek1337 <mikekegnlyew@gmail.com>
2022-07-26 18:37:11 +0300

    data

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * str_concate - concatenates two strings
+ * str_concat - concatenates two strings
  *
  * @s1: first string
  *
@@ -9,31 +11,34 @@
  *
  * Return: string
  */
-char *str_concate(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	 int s1_size = strlen(s1);
 	 int s2_size = strlen(s2);
 	 int total_size = s1_size + s2_size;
 	 int i;
 	 int j;
+	 char *concat;
 	total_size = total_size - 1;
-	if (s1 == NULL || S2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
-	 char *concat = malloc(sizeof(char) * total_size);
+	concat = malloc(sizeof(char) * total_size);
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < s1_size; i++)
 	{
+		if (s1[i] == '\0')
+			continue;
 		concat[i] = s1[i];
 	}
-	i++;
 	for (j = 0; j < s2_size; j++)
 	{
 		concat[i + j] = s2[j];
+		/*printf("%d",i + j);*/
 	}
 	return (concat);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,3 @@
-char *str_concate(char *s1, char *s2);
+char *str_concat(char *s1, char *s2);
 char *_strdup(char *s);
 char *create_array(unsigned int size, char c);
