7d6878f0fa0ad60a95a56d7f3a6b4134682abbaf
mikek1337 <mikekegnlyew@gmail.com>
2022-07-27 21:07:23 +0300

    whats my name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 always
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
