180a0dad2105ca08207a482f030ee4cc97f7c630
mikek1337 <mikekegnlyew@gmail.com>
2022-07-27 20:56:08 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: count number of arguments
+ * @argv: arguments passed
+ * retutn: 0 always
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is the argv and argc project
