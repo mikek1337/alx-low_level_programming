d09af814ffc87deaefa4862d4c331a33f11b2680
mikek1337 <mikekegnlyew@gmail.com>
2022-07-28 14:43:18 +0300

    common mistake sucks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,15 +5,24 @@
  * string_nconcat - concat strings
  * @s1: first string
  * @s2: second string
+ * @n: number of character copied
  * Return: char pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char * new_s;
-	unsigned int n1 = strlen(s1);
-	unsigned int n2 = strlen(s2);
-	unsigned int i;
-	unsigned int j;
+	 char *new_s;
+	 unsigned int i;
+	 unsigned int j;
+	 unsigned int n1;
+	 unsigned int n2;
+	if (s1 == NULL)
+		n1 = 0;
+	else
+		n1 = strlen(s1);
+	if (s2 == NULL)
+		n2 = 0;
+	else
+		n2 = strlen(s2);
 
 	if (n > n2)
 		n = n2;
@@ -22,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < n1; i++)
 	{
-		if(s1[i] == '\0')
+		if (s1[i] == '\0')
 			break;
 		new_s[i] = s1[i];
 	}
