be3df03ed0ef180f6a209f9d390c88cb33214f86
mikek1337 <mikekegnlyew@gmail.com>
2022-07-28 13:50:01 +0300

    check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks if malloc assigned memory
+ * @b: size of the memory
+ * Returns: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m;
+	m = malloc(b);
+	if (m == 0)
+		exit(98);
+	return (m);
+}
