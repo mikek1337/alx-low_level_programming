048fe29e99d9a73c44f1db3a47e835d511089756
mikek1337 <mikekegnlyew@gmail.com>
2022-08-01 19:30:06 +0300

    ABS macro arg

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,19 +1,19 @@
-#include "1-pi.h"
-#include "1-pi.h"
 #include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    float a;
-	        float r;
+    int i;
+    int j;
 
-		    r = 98;
-		        a = PI * r * r;
-			    printf("%.3f\n", a);
-			        return (0);
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
 }
