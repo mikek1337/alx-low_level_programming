bc500212585bda932bf1e5f75bad60caf101c8b2
mikek1337 <mikekegnlyew@gmail.com>
2022-08-01 07:24:51 -0400

    fixed dog.h style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
@@ -15,3 +17,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
