34eb4b0004e1b0e08ec5b2b859aab6582788ddbf
mikek1337 <mikekegnlyew@gmail.com>
2022-08-03 05:29:14 -0400

    to hell with circumstances; i create opportunities

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,11 @@
+/**
+ * struct op - Struct op
+ *
+ * @op: the operator
+ * @f: the function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of the number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the difference of the two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplys two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiple of the two number
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division of the two number
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: module of the two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
