2e83f58f303449af80f4e54ffba9edf66a5e013c
mikek1337 <mikekegnlyew@gmail.com>
2022-08-03 04:48:13 -0400

    to hell with circumstances; i create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search's element the array and returns it first occurence index
+ * @array: the array that is going to be searched
+ * @size: size of the array
+ * @cmp: a function that search's the array
+ * Return: on success the index of the array
+ * if size is less than 0 then -1
+ * if no match found -1
+ */
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	 int i;
+	 int match;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		match = cmp(array[i]);
+		if (match)
+			return (i);
+	}
+	return (-1);
+}
