517f08a02ffbd6d8210471c59af94b09f388ed90
mikek1337 <mikekegnlyew@gmail.com>
2022-08-11 20:31:37 +0300

    linked list display

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * print - prints lists
+ * @h: the list
+ * @count: number of lists
+ * Return: number of count
+ */
+size_t print(const list_t *h, int count)
+{
+	 char *s;
+	if (h == NULL)
+		return (count);
+	if (h->str == NULL)
+		s = "(nil)";
+	else
+		s = h->str;
+	_putchar('[');
+	_putchar((h->len % 10) + '0');
+	_putchar(']');
+	_putchar(' ');
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+	h = h->next;
+	count++;
+	return (print(h, count));
+}
+
+/**
+ * print_list - print all the elements of list_t
+ * @h: the list passed
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	return (print(h, 0));
+}
