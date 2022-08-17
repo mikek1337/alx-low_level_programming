dbdda76f70ad90c766c7a435280161faa519ada0
mikek1337 <mikekegnlyew@gmail.com>
2022-08-17 20:05:23 +0300

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,23 +5,16 @@
 /**
  * add_nodeint - adds node to the beginning of the list
  * @head: the head of the list
- * @str: the string that needs to be added
+ * @n: the number that needs to be added
  * Return: list pointer
  */
-list_t *add_nodeint(list_t **head, char *str)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	 list_t *node;
-	 unsigned int count = 0;
-	node = malloc(sizeof(list_t));
+	 listint_t *node;
+	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
-	node->str = strdup(str);
-	while (*str != '\0')
-	{
-		str++;
-		count++;
-	}
-	node->len = count;
+	node->n = n;
 	if (head == NULL)
 		node->next = NULL;
 	else
