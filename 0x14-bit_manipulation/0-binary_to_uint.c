f84f4b9f9741b424746eb04714c1eca9bf938dd7
mikek1337 <mikekegnlyew@gmail.com>
2022-08-19 22:52:15 +0300

    indentation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	 unsigned int n = 0;
-	 int len = 0;
-	 int i = 0;
-	if (b == NULL)
-		return (0);
-	while (b[len])
-		len++;
-	while (len != i)
-	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
+	 	 unsigned int n = 0;
+	 	 int len = 0;
+	 	 int i = 0;
+		if (b == NULL)
 			return (0);
-		i++;
-	}
-	return (n);
+		while (b[len])
+			len++;
+		while (len != i)
+		{
+			if (b[len - i - 1] == '1')
+				n += 1 << i;
+			else if (b[len - i - 1] != '0')
+				return (0);
+			i++;
+		}
+		return (n);
 }
