6a3911598a614a3ad4a0d30b5433e0c979da44b3
mikek1337 <mikekegnlyew@gmail.com>
2022-08-19 22:03:40 +0300

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - changes binary into decimal
+ * @b: the binary data
+ * return: decimal
+ * 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int len = 0;
+	int i = 0;
+	if (b == NULL)
+		return (0);
+	while (b[len])
+		len++;
+	while (len != i)
+	{
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
+			return (0);
+		i++;
+	}
+	return (n);
+}
