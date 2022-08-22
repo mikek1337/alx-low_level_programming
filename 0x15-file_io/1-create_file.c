b3697f7e48bbfb45d7db3ec84218a7bfc3a03edd
mikek1337 <mikekegnlyew@gmail.com>
2022-08-22 22:31:28 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,17 @@ int create_file(const char *filename, char *text_content)
 {
 	 int fd;
 	 int wcount;
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	 int i;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1 || filename == NULL)
 		return (-1);
-	wcount = write(fd, text_content, sizeof(text_content) - 1);
-	if (wcount == -1)
-		return (-1);
+	if (text_content)
+	{
+		for(i = 0; text_content[i] != '\0'; i++);
+		wcount = write(fd, text_content, i);
+		if (wcount == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
