066528a95cc472269c92521ce818931485cc9266
mikek1337 <mikekegnlyew@gmail.com>
2022-08-22 22:43:23 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for(i = 0; text_content[i] != '\0'; i++);
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		wcount = write(fd, text_content, i);
 		if (wcount == -1)
 			return (-1);
