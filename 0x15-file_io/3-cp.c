eb37035074b0ef8a1a3a88da199abdd59ae80ea0
mikek1337 <mikekegnlyew@gmail.com>
2022-08-22 22:53:44 +0300

    cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<stdlib.h>
 #define MAXSIZE 1204
 #define SE STDERR_FILENO
 
