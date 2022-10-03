a6a86047c024f527ad5a197fc5bf56898852408c
mikek1337 <mikekegnlyew@gmail.com>
2022-10-03 21:08:45 +0300

    operations

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,12 +1,60 @@
 #include "main.h"
 /**
- * cops - adds two numbers
+ * add - adds two numbers
  * @a: first number
  * @b: second number
  * Returns: sum of two numbers
  */
 
-int cops(int a, int b)
+int add(int a, int b)
 {
     return (a + b);
+}
+
+/**
+ * sub - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Returns: subtract of two numbers
+ */
+
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Returns: multiply of two numbers
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Returns: divide of two numbers
+ */
+
+double div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Returns: module of two numbers
+ */
+
+int mod(int a, int b)
+{
+    return (a % b);
 }
