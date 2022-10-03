e415999bbb82aac38818cd18883ae6f694b18cbb
mikek1337 <mikekegnlyew@gmail.com>
2022-10-03 21:00:06 +0300

    operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * cops - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Returns: sum of two numbers
+ */
+
+int cops(int a, int b)
+{
+    return (a + b);
+}
