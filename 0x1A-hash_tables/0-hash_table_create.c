08a3d6da121fcea4915423e54165cef61107ed78
mikek1337 <mikekegnlyew@gmail.com>
2022-10-20 22:30:19 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,5 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
+	{
+		return (NULL);
+	}
 	return (new_table);
 }
