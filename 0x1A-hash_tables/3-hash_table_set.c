369bbfbfc367d69325d9e29c5bd2f9a72b01d3bd
mikek1337 <mikekegnlyew@gmail.com>
2022-10-22 00:22:21 +0300

    free me

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,16 +10,39 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
+	 char *value_copy;
+	 unsigned long int i;
 	 unsigned long int index = 0;
+	
 	 hash_node_t *new_data = malloc(sizeof(hash_node_t *));
 	if (key == NULL || *key == '\0' || value == NULL || ht == NULL)
 		return (0);
-	if (new_data == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
+	if (new_data == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
 	index = key_index((unsigned char *)key, ht->size);
-	new_data->key = (char *)key;
-	new_data->value = strdup(value);
+	
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+	new_data->key = strdup(key);
+	new_data->value = value_copy;
+	if(new_data->key == NULL)
+	{
+		free(new_data);
+		return (0);
+	}
 	if (ht->array[index] != NULL)
 	{
 		new_data->next = ht->array[index];
